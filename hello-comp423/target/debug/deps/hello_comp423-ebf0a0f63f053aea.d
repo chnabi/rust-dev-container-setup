4c5bf5d73f61253f12c6b790694423c3b26fd017
Nabiha Choudhury <chnabi@unc.edu>
2025-01-27 01:23:03 +0000

    Followed Yohan's tutorial

---
+++ hello-comp423/target/debug/deps/hello_comp423-ebf0a0f63f053aea.d
@@ -0,0 +1,5 @@
+/workspaces/rust-dev-container-setup/hello-comp423/target/debug/deps/hello_comp423-ebf0a0f63f053aea: src/main.rs
+
+/workspaces/rust-dev-container-setup/hello-comp423/target/debug/deps/hello_comp423-ebf0a0f63f053aea.d: src/main.rs
+
+src/main.rs:
---
+++ hello-comp423/target/debug/hello-comp423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-dev-container-setup/hello-comp423/target/debug/hello-comp423: /workspaces/rust-dev-container-setup/hello-comp423/src/main.rs
